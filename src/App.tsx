a0cd0afcbff464d79871004c01b5fdad93bc0c07
makstravm <maks7-2-94@gmail.com>
2021-05-10 22:27:59 +0300

    creating a component, using props

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
 import React from 'react';
 import './App.css';
+import { TodoList } from './Todolist';
+
+
+export type TaskType = {
+  id: number
+  title: string
+  isDone: boolean
+}
 
 function App() {
+
+  //BLL:
+
+  const TasksToLearn: Array<TaskType> = [
+    { id: 1, title: 'HTML&CSS', isDone: true },
+    { id: 2, title: 'JS', isDone: false },
+    { id: 3, title: 'React', isDone: false }
+  ]
+  const TasksToBuy: Array<TaskType> = [
+    { id: 1, title: 'Bear', isDone: true },
+    { id: 2, title: 'Snacks', isDone: true },
+    { id: 3, title: 'Vodka', isDone: false }
+  ]
+  const TasksToRead: Array<TaskType> = [
+    { id: 1, title: 'Type Script', isDone: false },
+    { id: 2, title: 'Refactoring', isDone: false },
+    { id: 3, title: 'JavaScript for Kids', isDone: true }
+  ]
+
+  //UI:
+
   return (
     <div className="App">
-      <div>
-        <h3>What to learn</h3>
-        <div>
-          <input />
-          <button>+</button>
-        </div>
-        <ul>
-          <li><input type="checkbox" checked={true} /> <span>HTML&CSS</span></li>
-          <li><input type="checkbox" checked={true} /> <span>JS</span></li>
-          <li><input type="checkbox" checked={false} /> <span>React</span></li>
-        </ul>
-        <div>
-          <button>All</button>
-          <button>Active</button>
-          <button>Completed</button>
-        </div>
-      </div>
+      <TodoList title='What to learn' tasks={TasksToLearn}/>
+
+      <TodoList title='What to buy' tasks={TasksToBuy}/>
+
+      <TodoList title='What to read' tasks={TasksToRead}/>
+
     </div>
   );
 }
