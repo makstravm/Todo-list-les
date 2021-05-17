0c3462b42b7f1ccb7ed602e1049247adf7e3db85
makstravm <maks7-2-94@gmail.com>
2021-05-17 21:47:17 +0300

    added change input checkbox, input field handler for space and input error, styling completed tasks

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -7,24 +7,48 @@ type PropsTodoList = {
   removeTask: (taskId: string) => void
   changeFilterTodoList: (value: filterValuesType) => void
   addTask: (title: string) => void
+  filter: filterValuesType
+  changeTaskStatus: (taskId: string, isDone: boolean) => void
 }
 export function TodoList(props: PropsTodoList) {
   const [title, setTitle] = useState<string>('')
+  const [error, setError] = useState<boolean>(false)
   const tasksJSX = props.tasks.map(t => {
+    const taskClass = 'listSpan' + ' ' + (t.isDone ? 'is-done' : '')
     const removeTask = () => props.removeTask(t.id)
+    const changeCheckBox = (e: ChangeEvent<HTMLInputElement>) => {
+      props.changeTaskStatus(t.id, e.currentTarget.checked)
+    }
     return (
-      <li >
-        <input type="checkbox" checked={t.isDone} />
-        <span>{t.title}</span>
-        <button onClick={removeTask}>X</button>
+      <li key={t.id} className='listItem'>
+        <label>
+          <input
+            className='checkboxInput'
+            type="checkbox" checked={t.isDone}
+            onChange={changeCheckBox}
+          />
+          <span className='checkboxCheck'></span>
+        </label>
+        <span className={taskClass}>{t.title}</span>
+        <button onClick={removeTask}
+          className='btnInput'>x</button>
       </li>
     )
   })
   const onClickAddTask = () => {
-    props.addTask(title)
+    const validetedTitle = title.trim()
+    if (validetedTitle) {
+      props.addTask(validetedTitle)
+    } else {
+      setError(true)
+    }
     setTitle('')
   }
-  const onChangeTitle = (e: ChangeEvent<HTMLInputElement>) => setTitle(e.currentTarget.value)
+  const errorStatus = error ? 'error' : ''
+  const onChangeTitle = (e: ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.currentTarget.value)
+    setError(false)
+  }
   const onKeyPressAddTask = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
       onClickAddTask()
@@ -34,6 +58,10 @@ export function TodoList(props: PropsTodoList) {
   const changeFilterActiveTodoList = () => props.changeFilterTodoList('active')
   const changeFilterCompletedTodoList = () => props.changeFilterTodoList('completed')
 
+  const activeFilterBtnAll = 'btn' + ' ' + (props.filter === 'all' ? 'active-filter' : '')
+  const activeFilterBtnActive = 'btn' + ' ' + (props.filter === 'active' ? 'active-filter' : '')
+  const activeFilterBtnCompleted = 'btn' + ' ' + (props.filter === 'completed' ? 'active-filter' : '')
+
   return (
     <div>
       <h3>{props.title}</h3>
@@ -42,16 +70,21 @@ export function TodoList(props: PropsTodoList) {
           value={title}
           onChange={onChangeTitle}
           onKeyPress={onKeyPressAddTask}
+          className={'input' + ' ' + (errorStatus)}
         />
-        <button onClick={onClickAddTask}>+</button>
+
+        <button
+          className='btnInput'
+          onClick={onClickAddTask}>+</button>
+        {error && <div className='errorText'>Text isn't corrected!!!</div>}
       </div>
-      <ul>
-        <li>{tasksJSX}</li>
+      <ul className='list'>
+        {tasksJSX}
       </ul>
       <div>
-        <button onClick={changeFilterAllTodoList}>All</button>
-        <button onClick={changeFilterActiveTodoList}>Active</button>
-        <button onClick={changeFilterCompletedTodoList}>Completed</button>
+        <button className={activeFilterBtnAll} onClick={changeFilterAllTodoList}>All</button>
+        <button className={activeFilterBtnActive} onClick={changeFilterActiveTodoList}>Active</button>
+        <button className={activeFilterBtnCompleted} onClick={changeFilterCompletedTodoList}>Completed</button>
       </div>
     </div >
   )
